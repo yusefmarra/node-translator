3a9b9ea315d77db3c2ecef101076182bee940765
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-04 10:56:51 -0600

    Add client side input

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,4 +1,7 @@
 var token, random, langs;
+var langCodes = '["ar", "bs-Latn", "bg", "ca", "zh-CHS", "zh-CHT", "hr", "cs", "da", "nl", "en", "et", "fi", "fr", "de", "el", "ht", "he", "hi", "mww", "hu", "id", "it", "ja", "tlh", "tlh-Qaak", "ko", "lv", "lt", "ms", "mt", "yua", "no", "otq", "fa", "pl", "pt", "ro", "ru", "sr-Cyrl", "sr-Latn", "sk", "sl", "es", "sv", "th", "tr", "uk", "ur", "vi", "cy"]';
+var codeArray = ["ar", "bs-Latn", "bg", "ca", "zh-CHS", "zh-CHT", "hr", "cs", "da", "nl", "en", "et", "fi", "fr", "de", "el", "ht", "he", "hi", "mww", "hu", "id", "it", "ja", "tlh", "tlh-Qaak", "ko", "lv", "lt", "ms", "mt", "yua", "no", "otq", "fa", "pl", "pt", "ro", "ru", "sr-Cyrl", "sr-Latn", "sk", "sl", "es", "sv", "th", "tr", "uk", "ur", "vi", "cy"];
+
 $(window).on('load',function() {
   console.log('loaded');
   getToken();
@@ -6,9 +9,9 @@ $(window).on('load',function() {
   setTimeout(languages, 200);
 });
 
-function createList(items) {
+function createList(items, codes) {
   for (var i = 0; i < items.length; i++) {
-    $('.dropdown-menu').append('<li><a href="#">' + langs[i] + '</a></li>');
+    $('.lang-list').append('<option value="' + codes[i] + '">' + items[i] + '</option>');
   }
 }
 
@@ -76,11 +79,11 @@ function languages() {
     data: {
       appId: 'Bearer ' + token,
       locale: 'en',
-      languageCodes: '["ar", "bs-Latn", "bg", "ca", "zh-CHS", "zh-CHT", "hr", "cs", "da", "nl", "en", "et", "fi", "fr", "de", "el", "ht", "he", "hi", "mww", "hu", "id", "it", "ja", "tlh", "tlh-Qaak", "ko", "lv", "lt", "ms", "mt", "yua", "no", "otq", "fa", "pl", "pt", "ro", "ru", "sr-Cyrl", "sr-Latn", "sk", "sl", "es", "sv", "th", "tr", "uk", "ur", "vi", "cy"]'
+      languageCodes: langCodes
     }
   }).done(function(data) {
     langs = JSON.parse(data);
-    setTimeout(createList(langs), 400);
+    setTimeout(createList(langs, codeArray), 500);
   }).fail(function(error) {
     console.log(error.status);
   });
